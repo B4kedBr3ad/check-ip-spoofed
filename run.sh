6240e19561cfbbb97e110e06cc1f2e30226173b6
B4kedBr3ad <110211701+B4kedBr3ad@users.noreply.github.co>
2023-02-26 14:51:12 +0900

    Add: wehere change

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ip=$(curl -s  http://checkip.amazonaws.com/)
-
+# set your global IP adrr
 if [ "$ip" != "0.0.0.0" ]; then
     echo "\033[32mOK\033[0m"
 else
-    echo "\033[31m=\n!!!ORIGIN DETECTED!!!\n=\033[0m"
+    echo "\033[31m=\n!!!NOT SPOOFED!!!\n=\033[0m"
 fi
