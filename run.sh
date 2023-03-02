bdb0cb01f3926eee27e3e71050a8c94f19104404
B4kedBr3ad <110211701+B4kedBr3ad@users.noreply.github.com>
2023-03-02 09:22:06 +0900

    Change: change somethings

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 ip=$(curl -s  http://checkip.amazonaws.com/)
 # set your global IP adrr
 if [ "$ip" != "0.0.0.0" ]; then
-    echo "\033[32mOK\033[0m"
+    echo "\033[32m--- OK ---\033[0m"
 else
-    echo "\033[31m=\n!!!NOT SPOOFED!!!\n=\033[0m"
+    echo "\033[31m=\n!!! NOT SPOOFED !!!\n=\033[0m"
 fi
